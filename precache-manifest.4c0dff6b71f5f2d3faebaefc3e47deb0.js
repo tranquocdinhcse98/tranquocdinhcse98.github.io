68ca867f898df2ebfc0de9a0794d950488b8f05a
Dinh Tran <tranquocdinhcse98@outlook.com>
2020-08-18 18:50:15 +0700

    Updates

---
+++ precache-manifest.4c0dff6b71f5f2d3faebaefc3e47deb0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7894846bccb18dfddab8bceaf25ca80c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "139c9e1f5280b4072602",
+    "url": "/static/css/main.1d799bf3.chunk.css"
+  },
+  {
+    "revision": "9c457a65128806ee9912",
+    "url": "/static/js/2.d0249ff4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d0249ff4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "139c9e1f5280b4072602",
+    "url": "/static/js/main.41352e03.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  },
+  {
+    "revision": "c71f55cf72cb813b4e3451d06abb05f2",
+    "url": "/static/media/avatar.c71f55cf.jpg"
+  },
+  {
+    "revision": "5bf7e95784b17397b51a6db8df7002fc",
+    "url": "/static/media/avatar2.5bf7e957.jpg"
+  },
+  {
+    "revision": "80df5d7c49fa9c3413cb03e5ee117381",
+    "url": "/static/media/background1.80df5d7c.jpg"
+  },
+  {
+    "revision": "841a5cdbf982a7bc4bd4f484fcd8da72",
+    "url": "/static/media/project_1 (2).841a5cdb.png"
+  },
+  {
+    "revision": "9a60534e6d648f2cba891dc41eb80817",
+    "url": "/static/media/project_2.9a60534e.png"
+  },
+  {
+    "revision": "e3ddaf5dc77133ecaf4f1d4221d54d0c",
+    "url": "/static/media/project_3 (3).e3ddaf5d.png"
+  }
+]);
